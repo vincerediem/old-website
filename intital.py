0f33efc951f08a0ae64d664abf353c0c60e4251e
Keith <kopenshaw@me.com>
2023-06-30 22:15:44 -0700

    made changes reccomended

--- intital.py
+++ intital.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request
+import sys
+sys.path.append(r"C:\Users\kopen\OneDrive\Desktop\Code\Algo Trading\RSI\RSI 2")
+import RSIv2_basefucntions as rsi2
 
 app = Flask(__name__)
 
@@ -8,16 +11,14 @@ def index():
 
 @app.route('/run_script', methods=['POST'])
 def run_script():
-    param1 = request.form.get('param1')
-    param2 = request.form.get('param2')
-    # Perform calculations or call your existing Python script with the received parameters
-    result = perform_calculations(param1, param2)
+    stock_symbols = request.form.get('stock_symbols') 
+    result = perform_calculations(stock_symbols)
     return render_template('result.html', result=result)
 
-def perform_calculations(param1, param2):
-    # Your Python code that uses the input parameters
-    # Return the result
-    return param1 + param2
+def perform_calculations(stock_symbols):
+    final_balance, initial_balance, stock, row, positions, cash, trade_gains_losses = rsi2.backtest_strategy(rsi2.stock_list(stock_symbols))
+    results = rsi2.display_final_metrics(final_balance, initial_balance, stock, row, positions, cash, trade_gains_losses)
+    return results  # You'll need to adjust the display_final_metrics function to return results instead of print
 
 if __name__ == '__main__':
     app.run(debug=True)
